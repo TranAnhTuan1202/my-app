25801df8e81abe7340ac91c367c95e118e5feac6
TranAnhTuan1202 <trananhtuan120299@gmail.com>
2023-03-24 15:43:03 +0700

    test 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Content from './pages/Content';
 
 function App() {
   return (
     <div >
-      HOME PAGE
+      <Home/>
+      <Content/>
       
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@
 function Home() {
   return (
     <div >
-      test 
+      x
 
     </div>
   );
